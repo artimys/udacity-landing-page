b424bc229262aa9dc177af183df73dba74ad6b6a
Arturo Simon <artysimon@gmail.com>
2022-05-26 23:47:33 -0400

    Add scroll to section panel

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const data = [
         link: "Section 1",
         title: "Section 1",
         content: `
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo molestiae ratione ipsum necessitatibus rerum, voluptatem obcaecati repellat praesentium ullam ad eveniet omnis voluptates itaque deserunt ducimus aliquam quasi incidunt quibusdam!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
             Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
             Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
@@ -14,6 +15,7 @@ const data = [
         link: "Section 2",
         title: "Section 2",
         content: `
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quisquam sunt quis error odio voluptates non porro, veritatis reprehenderit numquam fugiat itaque. Velit libero earum architecto fugiat nemo, unde consequuntur.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
             Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
             Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
@@ -24,6 +26,7 @@ const data = [
         link: "Section 3",
         title: "Section 3",
         content: `
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quae, aliquam hic ut esse non magni sequi dolorem. Ratione fuga voluptas molestiae distinctio illo voluptate aliquid vero natus, illum eaque.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
             Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
             Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
@@ -37,13 +40,22 @@ const data = [
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
             Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
             Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
+            Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
+            Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cum quaerat facilis excepturi, rem numquam corrupti tempore quas beatae omnis suscipit doloribus aut dignissimos sit assumenda non voluptas cupiditate qui?
+            Incidunt, ut reiciendis voluptate amet provident debitis numquam tenetur rem fugiat quod deserunt saepe earum assumenda ad, facere laudantium enim autem mollitia vero cum atque animi. Magnam dolorum soluta nobis?
+            Omnis dicta aperiam, dolor iure atque consequatur eum cum quisquam autem deleniti voluptatum tempora architecto voluptate reprehenderit hic magnam aspernatur doloribus nesciunt vitae velit, ipsum natus incidunt modi tempore! Eos!  Laborum dolores nulla sed. Aperiam minima cum repellendus fugit nihil et iste iusto ipsam non mollitia obcaecati, sapiente minus voluptates rem magnam numquam sequi suscipit expedita nisi quod placeat molestiae!
             </p>`
     }
 ]
 
-let sectionList = document.querySelector("#section-list");
+const sectionList = document.querySelector("#section-list");
 const navbarList = document.querySelector(".navbar-list");
-const scrollToTop = document.querySelector("#scroll-to-top");
+const scrollToTop = document.querySelector(".scroll-to-top");
+const navbarLinks = document.querySelectorAll(".navbar__link");
 
 
 /* INSERT DATA TO DOM */
@@ -57,7 +69,8 @@ function addSectionToNavbar(section) {
 
     // Create <a>
     const a = document.createElement("a");
-    a.href = `#panel-${section.id}`;
+    a.href = "#" ;
+    a.dataset.id = `#panel-${section.id}`;
     a.className = "navbar__link";
     a.textContent = section.link;
 
@@ -87,46 +100,89 @@ function addSectionToPanel(section) {
 
 
 
+/* HELPER FUNCTIONS */
+function resetNavbarLinks() {
+    const navbarLinks = document.querySelectorAll(".navbar__link");
+
+    // Remove active state for any navbar links
+    navbarLinks.forEach(function(link) {
+        link.classList.remove("navbar__link--active");
+    });
+}
+
+
 /* COLLAPSE PANEL EVENT */
 function togglePanel(event) {
     event.preventDefault();
 
     if (event.target.className == "panel__toggle") {
         const panel = document.querySelector("#" + event.target.dataset.id);
+
+        // Remove active state for any navbar links
+        resetNavbarLinks();
+
+        // Add/remove class to collapse panel
         panel.classList.toggle("panel--collapse");
+
+        // if (panel.classList.contains("panel--collapse")) {
+        //     panel.classList.remove("panel--collapse");
+        // } else {
+        //     panel.classList.add("panel--collapse");
+        // }
     }
-    // let panel_toggle = document.querySelectorAll(".panel__toggle");
-    // panel_toggle.addEventListener("click",  function(event) {
-    //     event.preventDefault();
-    //         console.log("adsf");
-    // });
+}
+
+
+
+/* SCROLL TO SECTION (PANEL) */
+function scrollToPanel(event) {
+    event.preventDefault();
+    const navbarLink = event.target;
+    const sectionPanel = document.querySelector(navbarLink.dataset.id);
+
+    // Remove active state for any navbar links
+    resetNavbarLinks();
+
+    // Set active state to current link
+    navbarLink.classList.add("navbar__link--active");
+
+    // Expand panel in case it is collapsed
+    sectionPanel.classList.remove("panel--collapse");
+
+    /*
+    This scrolls just after the element,
+    Nice CSS `scroll-margin-top` compliments the offset
+    https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-margin-top
+    https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView */
+    sectionPanel.scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
 
 
 /* SCROLL TO TOP */
 function scrollToTopFadeIn() {
-    if (window.scrollY > 200) {
-        scrollToTop.style.visibility = "visible";
+    if (window.pageYOffset > 300) {
+        scrollToTop.classList.add("scroll-to-top--visible");
     } else {
-        scrollToTop.style.visibility = "hidden";
+        scrollToTop.classList.remove("scroll-to-top--visible");
     }
 }
 
 function scrollUpToHeader() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll
     window.scroll({
         top: 0,
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth"
     });
 }
 
 
 
-
-
 /* PAGE LOAD SETUP */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     for (const section of data) {
         // Add link to navbar
         addSectionToNavbar(section);
@@ -135,7 +191,7 @@ document.addEventListener('DOMContentLoaded', function () {
         addSectionToPanel(section)
     }
 
-    // Event to trigger collapse panel toggle
+    // Event to toggle collapse panel
     sectionList.addEventListener("click", togglePanel);
 
     // Scroll window event
@@ -143,6 +199,12 @@ document.addEventListener('DOMContentLoaded', function () {
         // Make scroll btn appear after scrolling
         scrollToTopFadeIn();
     });
+
+    // Scroll to panel
+    const navbarLinks = document.querySelectorAll(".navbar__link");
+    for (const link of navbarLinks) {
+        link.addEventListener("click", scrollToPanel);
+    }
 
     // Scroll back to header
     scrollToTop.addEventListener("click", scrollUpToHeader);
